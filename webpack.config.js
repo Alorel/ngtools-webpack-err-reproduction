e8cfb65c8cdb44d4fa0f7771bc632f240c1d83bb
Arturas <a.molcanovas@gmail.com>
2018-10-23 18:33:19 +0100

    error is thrown

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const {join} = require('path');
 const {AngularCompilerPlugin} = require('@ngtools/webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const srcDir = join(__dirname, 'src');
 
@@ -10,23 +11,54 @@ module.exports = {
   entry: join(srcDir, 'index.ts'),
   output: {
     path: join(__dirname, 'dist'),
+    chunkFilename: '[name].js',
     filename: 'bundle.js',
     publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        default: {
+          priority: -5
+        },
+        vendor: {
+          priority: -4,
+          test: /node_modules/
+        }
+      },
+      chunks: 'all',
+      maxAsyncRequests: Number.MAX_VALUE,
+      maxInitialRequests: Number.MAX_VALUE,
+      minChunks: 1,
+      minSize: 1
+    },
   },
   plugins: [
     new AngularCompilerPlugin({
       tsConfigPath: join(__dirname, 'tsconfig.json')
-    })
+    }),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: '.gitignore',
+          to: '.',
+          toType: 'dir',
+          cache: true
+        }
+      ],
+      {context: 'src/'}
+    )
   ],
   resolve: {
     mainFields: ['fesm5', 'esm5', 'module', 'browser', 'main'],
     extensions: ['.ts', '.js', '.json']
   },
   module: {
-    rules: [{
-      test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
-      // test: /\.[jt]s$/,
-      use: ['@ngtools/webpack']
-    }]
+    rules: [
+      {
+        test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+        use: ['@ngtools/webpack']
+      }
+    ]
   }
 };
